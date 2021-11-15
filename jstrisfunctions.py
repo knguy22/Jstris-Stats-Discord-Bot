1e6c04f150de64226db0fd6ebbc31f9d5d146859
doublesmokedbaconandpie <77951761+doublesmokedbaconandpie@users.noreply.github.com>
2021-11-15 09:05:58 -0800

    Add files via upload

--- jstrisfunctions.py
+++ jstrisfunctions.py
@@ -2,6 +2,126 @@ import jstrisuser
 import datetime
 
 
+# Returns self.game, self.mode, self.period, self.param
+# self.game, self.mode, and self.period are all strings of integers
+# self.param is preserved as a string
+# Ex: self.game = 1, self.mode = 2, self.period = 3
+# self.param = finesse
+class ParameterInit:
+    valid_params = True
+    gamemode = ""
+    game = ""
+    period = ""
+    mode = ""
+    param = ""
+
+    def __init__(self, my_tuple):
+        self.valid_params = True
+        self.gamemode = ""
+        self.game = ""
+        self.period = ""
+        self.mode = ""
+        self.param = ""
+
+        # NOTE: MY TUPLE MUST CONTAIN MORE THAN ONE INDEX FOR THIS TO WORK; ADD AN EXTRA EMPTY STRING IN THE
+        # TUPLE IF NEEDED
+        # checking for all settings in my_tuple
+
+        for i in my_tuple:
+            self.gamemode_init(i)
+            self.period_str_to_int(i)
+
+        for i in my_tuple:
+            self.param_init(i, self.gamemode)
+
+        # sets defaults for unspecified settings in my_tuple
+
+        self.default_settings()
+        print(self.game, self.mode, self.param, self.period, self.gamemode)
+
+    def period_str_to_int(self, my_str):
+        if my_str in ('day', 'Day', 'today', 'Today'):
+            self.period = '1'
+        elif my_str in ("week", 'Week'):
+            self.period = '2'
+        elif my_str in ("month", "Month"):
+            self.period = "3"
+        elif my_str in ("year", "Year"):
+            self.period = '4'
+        elif my_str in ('alltime', "Alltime"):
+            self.period = '0'
+
+    def param_init(self, my_param, game):
+        if game in ('ultra', 'Ultra') and my_param in ("ppb", 'PPB', 'Ppb'):
+            self.param = 'ppb'
+        if game in ('ultra', 'Ultra') and my_param in ("score", 'Score'):
+            self.param = 'score'
+        if game in ("pcmode", "PCmode") and my_param in ('pcs', 'PCS', 'PC', 'Pc', 'Pcs', 'pc'):
+            self.param = 'pcs'
+        if game in ("20tsd", "20TSD") and my_param in ('tsds', 'TSDS', 'Tsds'):
+            self.param = 'tsds'
+        if my_param in ('pps', 'PPS', 'Pps'):
+            self.param = 'pps'
+        if my_param in ('blocks', 'Blocks'):
+            self.param = 'blocks'
+        if my_param in ('finesse', 'Finesse'):
+            self.param = 'finesse'
+        if my_param in ('time', 'Time'):
+            self.param = 'time'
+
+    def gamemode_init(self, my_str):
+        a = False
+        if my_str == "sprint":
+            a = {"game": '1', "mode": "1", "gamemode": 'sprint'}
+        elif my_str == "sprint20":
+            a = {"game": '1', "mode": "2", "gamemode": 'sprint'}
+        elif my_str == "sprint40":
+            a = {"game": '1', "mode": "1", "gamemode": 'sprint'}
+        elif my_str == "sprint100":
+            a = {"game": '1', "mode": "3", "gamemode": 'sprint'}
+        elif my_str == "sprint1000":
+            a = {"game": '1', "mode": "1", "gamemode": 'sprint'}
+        elif my_str == "cheese":
+            a = {"game": '3', "mode": "3", "gamemode": 'cheese'}
+        elif my_str == "cheese10":
+            a = {"game": '3', "mode": "1", "gamemode": 'cheese'}
+        elif my_str == "cheese18":
+            a = {"game": '3', "mode": "2", "gamemode": 'cheese'}
+        elif my_str == "cheese100":
+            a = {"game": '3', "mode": "3", "gamemode": 'cheese'}
+        elif my_str == "survival":
+            a = {"game": '4', "mode": "1", "gamemode": 'survival'}
+        elif my_str == "ultra":
+            a = {"game": '5', "mode": "1", "gamemode": 'ultra'}
+        elif my_str == '20tsd':
+            a = {"game": '7', "mode": "1", "gamemode": '20tsd'}
+        elif my_str == "pcmode":
+            a = {"game": '8', "mode": "1", "gamemode": 'pcmode'}
+        if a:
+            self.game = a["game"]
+            self.mode = a["mode"]
+            self.gamemode = a["gamemode"]
+
+    def default_settings(self):
+        if self.gamemode == "":
+            self.game = '1'
+            self.mode = '1'
+
+        if self.period == "":
+            self.period = "alltime"
+            self.period = 0
+
+        if self.param == "":
+            if self.gamemode in 'ultra':
+                self.param = "score"
+            elif self.gamemode in "20tsd":
+                self.param = "tsds"
+            elif self.gamemode in 'pc':
+                self.param = "pcs"
+            else:
+                self.param = "time"
+
+
 def sub300(listofruns):
     c = 0
     for i in listofruns:
@@ -56,10 +176,21 @@ def most_(list_of_runs, my_param):
 
 def average_(list_of_runs, my_param):
 
+    if my_param == "time":
+        my_param = 'seconds'
+        list_of_seconds = list(map(lambda x: jstrisuser.clock_to_seconds(x['time']), list_of_runs))
+        c = 0
+        for my_second in list_of_seconds:
+            list_of_runs[c]['seconds'] = my_second
+            c += 1
+
     stat_average = 0
     for i in list_of_runs:
         stat_average += i[my_param]
-    return round(stat_average/len(list_of_runs), 3)
+
+    if my_param == "seconds":
+        return jstrisuser.seconds_to_clock(round(stat_average/len(list_of_runs), 2))
+    return round(stat_average/len(list_of_runs), 2)
 
 
 def pc_finish_sprint(list_of_runs, mode):
@@ -106,117 +237,52 @@ def num_games(list_of_runs):
     return len(list_of_runs)
 
 
-def period_str_to_int(my_str):
-    if my_str in ('day', 'Day', 'today', 'Today'):
-        return '1'
-    elif my_str in ("week", 'Week'):
-        return '2'
-    elif my_str in ("month", "Month"):
-        return "3"
-    elif my_str in ("year", "Year"):
-        return '4'
-    if my_str in ('alltime', "Alltime"):
-        return '0'
-    return False
+def livegames_avg(list_of_games, offset, param):
+    c = 0
+    summation = 0
+    while c < offset:
+        summation += list_of_games[c][param]
+        c += 1
+    return round(summation / offset, 2)
 
 
-def is_valid_param(my_param, game):
-    if game in ('ultra', 'Ultra') and my_param in ("ppb", 'PPB', 'Ppb'):
-        return 'ppb'
-    if game in ('ultra', 'Ultra') and my_param in ("score", 'Score'):
-        return 'score'
-    if game in ("pcmode", "PCmode") and my_param in ('pcs', 'PCS', 'PC', 'Pc', 'Pcs', 'pc'):
-        return 'pcs'
-    if game in ("20tsd", "20TSD") and my_param in ('tsds', 'TSDS', 'Tsds'):
-        return 'tsds'
-    if my_param in ('pps', 'PPS', 'Pps'):
-        return 'pps'
-    if my_param in ('blocks', 'Blocks'):
-        return 'blocks'
-    if my_param in ('finesse', 'Finesse'):
-        return 'finesse'
-    if my_param in ('time', 'Time'):
-        return 'time'
-    return False
+def games_won(list_of_games, offset):
+    c = 0
+    won_games = 0
+    while c < offset:
+        if list_of_games[c]["pos"] == 1:
+            won_games += 1
+        c += 1
+    return won_games
 
 
-def gamemode_init(my_str):
-    if my_str == "sprint":
-        return {"game": '1', "mode": "1"}
-    elif my_str == "sprint20":
-        return {"game": '1', "mode": "2"}
-    elif my_str == "sprint40":
-        return {"game": '1', "mode": "1"}
-    elif my_str == "sprint100":
-        return {"game": '1', "mode": "3"}
-    elif my_str == "sprint1000":
-        return {"game": '1', "mode": "1"}
-    elif my_str == "cheese":
-        return {"game": '3', "mode": "3"}
-    elif my_str == "cheese10":
-        return {"game": '3', "mode": "1"}
-    elif my_str == "cheese18":
-        return {"game": '3', "mode": "2"}
-    elif my_str == "cheese100":
-        return {"game": '3', "mode": "3"}
-    elif my_str == "survival":
-        return {"game": '4', "mode": "1"}
-    elif my_str == "ultra":
-        return {"game": '5', "mode": "1"}
-    elif my_str == '20tsd':
-        return {"game": '7', "mode": "1"}
-    elif my_str == "pcmode":
-        return {"game": '8', "mode": "1"}
-    else:
-        return False
+def first_last_date(list_of_games):
+    pass
 
 
-class ParameterInit:
-    valid_params = True
-    game = ""
-    period = ""
-    mode = ""
-    param = ""
+def opponents_matchups(list_of_games):
 
-    def __init__(self, my_parameter, period: str, gamemode: str):
-        a = gamemode_init(gamemode)
-        self.game = a["game"]
-        self.mode = a["mode"]
-        if not is_valid_param(my_parameter, gamemode):
-            self.valid_params = False
-        self.param = is_valid_param(my_parameter, self.game)
-        self.period = period_str_to_int(period)
+    list_of_opponents = {}
+    c = 0
+    while c < len(list_of_games):
+        if list_of_games[c]['players'] == 2:
+            if list_of_games[c]['vs'] not in list_of_opponents:
+                list_of_opponents[list_of_games[c]['vs']] = {"games": 1, "won": 0, "apm": 0, "spm": 0, "pps": 0}
+            else:
+                list_of_opponents[list_of_games[c]['vs']]['games'] += 1
 
+            if list_of_games[c]['pos'] == 1:
+                list_of_opponents[list_of_games[c]['vs']]['won'] += 1
+            list_of_opponents[list_of_games[c]['vs']]['apm'] += list_of_games[c]['apm']
+            list_of_opponents[list_of_games[c]['vs']]['spm'] += list_of_games[c]['spm']
+            list_of_opponents[list_of_games[c]['vs']]['pps'] += list_of_games[c]['pps']
 
-def parameter_init(my_tuple):
-    gamemode = False
-    period = False
-    my_parameter = False
+        c += 1
 
-    for i in my_tuple:
-        if gamemode_init(i):
-            gamemode = i
-        if period_str_to_int(i):
-            period = i
+    for key in list_of_opponents:
+        list_of_opponents[key]['apm'] = round(list_of_opponents[key]['apm'] / list_of_opponents[key]['games'], 2)
+        list_of_opponents[key]['spm'] = round(list_of_opponents[key]['spm'] / list_of_opponents[key]['games'], 2)
+        list_of_opponents[key]['pps'] = round(list_of_opponents[key]['pps'] / list_of_opponents[key]['games'], 2)
 
-    for i in my_tuple:
-        if is_valid_param(i, gamemode):
-            my_parameter = i
-
-    if not gamemode:
-        gamemode = "sprint"
-
-    if not period:
-        period = "alltime"
-
-    if not my_parameter:
-        if gamemode in ('ultra', 'Ultra'):
-            my_parameter = "score"
-        elif gamemode in ("20tsd", "20TSD"):
-            my_parameter = "tsds"
-        elif gamemode in ('pcs', 'PCS', 'PC', 'Pc', 'Pcs', 'pc'):
-            my_parameter = "pcs"
-        else:
-            my_parameter = "time"
-
-    return ParameterInit(my_parameter, period, gamemode)
+    # return list_of_opponents
+    return dict(sorted(list_of_opponents.items(), key=lambda x: x[1]['games'], reverse=True))
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import discord
-from jstrisuser import UserAllStats
-import jstrisfunctions
 from discord.ext import commands
+import jstrisfunctions
+from jstrisfunctions import ParameterInit
+from jstrisuser import UserIndivGames
+from jstrisuser import UserLiveGames
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 intents = discord.Intents.default()
 intents.members = True
-description = 'Third Party bot to quickly gather Jstris stats on individual players'
+description = 'Third party bot to quickly gather Jstris stats on individual players'
 
 bot = commands.Bot(command_prefix='?', description=description, intents=intents, help_command=None)
+loop = asyncio.get_event_loop()
 
 
 @bot.event
@@ -23,94 +28,232 @@ async def help(ctx):
 
 @bot.command()
 async def least(ctx, username, *args):
-    my_ps = jstrisfunctions.parameter_init(args)
+    my_ps = ParameterInit(args)
     if not my_ps.valid_params:
+        await ctx.send(ctx.author.mention)
         await ctx.send("Invalid parameter")
-    init_message = await ctx.send("Searching {}'s games now".format(username))
-    searched_games = UserAllStats(username, my_ps.game, my_ps.mode, my_ps.period)
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserIndivGames,
+                                                username, my_ps.game, my_ps.mode, my_ps.period)
     await init_message.delete()
-    if searched_games.has_error is True:
+    if searched_games.has_error:
         await ctx.send(searched_games.error_message)
     else:
         a = jstrisfunctions.least_(searched_games.all_stats, my_ps.param)
-        # await ctx.send("Least for {} is: ".format(my_ps.param) + str(a))
         await replay_send(ctx, a)
 
 
 @bot.command()
 async def most(ctx, username: str, *args):
-    my_ps = jstrisfunctions.parameter_init(args)
+    my_ps = ParameterInit(args)
     if not my_ps.valid_params:
+        await ctx.send(ctx.author.mention)
         await ctx.send("Invalid parameter")
-    init_message = await ctx.send("Searching {}'s games now".format(username))
-    searched_games = UserAllStats(username, my_ps.game, my_ps.mode, my_ps.period)
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserIndivGames,
+                                                username, my_ps.game, my_ps.mode, my_ps.period)
     await init_message.delete()
-    if searched_games.has_error is True:
+    if searched_games.has_error:
+        await ctx.send(ctx.author.mention)
         await ctx.send(searched_games.error_message)
     else:
         a = jstrisfunctions.most_(searched_games.all_stats, my_ps.param)
-        # await ctx.send("Most for {} is: ".format(my_ps.param) + str(a))
         await replay_send(ctx, a)
 
 
 @bot.command()
 async def average(ctx, username: str, *args):
-    my_ps = jstrisfunctions.parameter_init(args)
+    my_ps = ParameterInit(args)
     if not my_ps.valid_params:
+        await ctx.send(ctx.author.mention)
         await ctx.send("Invalid parameter")
-    init_message = await ctx.send("Searching {}'s games now".format(username))
-    searched_games = UserAllStats(username, my_ps.game, my_ps.mode, my_ps.period)
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserIndivGames,
+                                                username, my_ps.game, my_ps.mode, my_ps.period)
     await init_message.delete()
-    if searched_games.has_error is True:
+    if searched_games.has_error:
+        await ctx.send(ctx.author.mention)
         await ctx.send(searched_games.error_message)
     else:
         a = jstrisfunctions.average_(searched_games.all_stats, my_ps.param)
-        await ctx.send("Average for {} is: ".format(my_ps.param) + str(a))
-        # await replay_send(ctx, a)
+        await ctx.send(ctx.author.mention)
+        await ctx.send("Average {} for {} is: {}".format(my_ps.param, username, a))
 
 
 @bot.command()
 async def numgames(ctx, username: str, *args):
     # my_ps = ParameterInit(my_parameter, period, gamemode)
-    my_ps = jstrisfunctions.parameter_init(args)
-    init_message = await ctx.send("Searching {}'s games now".format(username))
-    searched_games = UserAllStats(username, my_ps.game, my_ps.mode, my_ps.period)
+    my_ps = ParameterInit(args)
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserIndivGames,
+                                                username, my_ps.game, my_ps.mode, my_ps.period)
     await init_message.delete()
-    if searched_games.has_error is True:
+    if searched_games.has_error:
         await ctx.send(searched_games.error_message)
     else:
         a = len(searched_games.all_stats)
+        await ctx.send(ctx.author.mention)
         await ctx.send("{} games".format(str(a)))
 
 
 @bot.command()
 async def sub300(ctx, username, period="alltime"):
-    init_message = await ctx.send("Searching {}'s games now".format(username))
-    period = jstrisfunctions.period_str_to_int(period)
-    searched_games = UserAllStats(username, "3", "3", period)
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    args = (period, '')
+    my_ps = ParameterInit(args)
+    period = my_ps.period
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserIndivGames,
+                                                username, "3", "3", period)
     await init_message.delete()
-    if searched_games.has_error is True:
+    if searched_games.has_error:
+        await ctx.send(ctx.author.mention)
         await ctx.send(searched_games.error_message)
     else:
-        await ctx.send("{} sub 300s".format(jstrisfunctions.sub300(searched_games.all_stats)))
+        await ctx.send(ctx.author.mention)
+        await ctx.send("{} has {} sub 300s".format(username, jstrisfunctions.sub300(searched_games.all_stats)))
+
+
+@bot.command()
+async def vs(ctx, username, offset=10):
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserLiveGames,
+                                                username, offset)
+    await init_message.delete()
+    if searched_games.has_error:
+        await ctx.send(ctx.author.mention)
+        await ctx.send("Invalid username: {}".format(username))
+        return None
+
+    # Calculates averages
+    apm_avg = jstrisfunctions.livegames_avg(searched_games.all_stats, offset, 'apm')
+    spm_avg = jstrisfunctions.livegames_avg(searched_games.all_stats, offset, 'spm')
+    pps_avg = jstrisfunctions.livegames_avg(searched_games.all_stats, offset, 'pps')
+    time_avg = jstrisfunctions.livegames_avg(searched_games.all_stats, offset, 'gametime')
+    players_avg = jstrisfunctions.livegames_avg(searched_games.all_stats, offset, 'players')
+    pos_avg = jstrisfunctions.livegames_avg(searched_games.all_stats, offset, 'pos')
+    won_games = jstrisfunctions.games_won(searched_games.all_stats, offset)
+
+    # Discord formatting
+    embed = await embed_init(username)
+    embed.add_field(name="**apm:**", value=apm_avg, inline=True)
+    embed.add_field(name="**spm:**", value=spm_avg, inline=True)
+    embed.add_field(name="**pps:**", value=pps_avg, inline=True)
+    embed.add_field(name="**time (seconds):**", value=time_avg, inline=True)
+    embed.add_field(name="**final position:**", value=pos_avg, inline=True)
+    embed.add_field(name="**players:**", value=players_avg, inline=True)
+    embed.add_field(name="**games won:**", value=str(won_games), inline=False)
+    embed.add_field(name="**number of games:**", value=str(offset), inline=False)
+    embed.set_footer(text='All of these values are averages.')
+    await ctx.send(ctx.author.mention)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def allmatchups(ctx, username):
+    offset = 10000000000
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserLiveGames,
+                                                username, offset)
+    await init_message.delete()
+    if searched_games.has_error:
+        await ctx.send(ctx.author.mention)
+        await ctx.send("Invalid username: {}".format(username))
+        return None
+    list_of_opponents = jstrisfunctions.opponents_matchups(searched_games.all_stats)
+
+    # Discord formatting stuff
+    # Get top 10 opponents
+
+    embed = await embed_init(username)
+
+    c = 0
+    for key in list_of_opponents:
+        if key is None:
+            continue
+        embed.add_field(name='**opponent:**', value="{}. {}".format(c + 1, key), inline=True)
+        embed.add_field(name='**games won (by user):**', value=list_of_opponents[key]["won"], inline=True)
+        embed.add_field(name='**total games:**', value=list_of_opponents[key]["games"], inline=True)
+        c += 1
+        if c >= 8:
+            break
+    embed.set_footer(text='Only the top 8 players with most played games are shown here for technical reasons. '
+                          'For individual match ups, try using ?indivmatchup instead. Also, Jstris will delete replays'
+                          'over time, and they will not be counted here.')
+
+    await ctx.send(ctx.author.mention)
+    await ctx.send(embed=embed)
+
+
+@bot.command()
+async def vsmatchup(ctx, username, opponent):
+    offset = 10000000000
+    init_message = await ctx.send("Searching {}'s games now. This can take a while.".format(username))
+    searched_games = await loop.run_in_executor(ThreadPoolExecutor(),
+                                                UserLiveGames,
+                                                username, offset)
+    await init_message.delete()
+    if searched_games.has_error:
+        await ctx.send(ctx.author.mention)
+        await ctx.send("Invalid username: {}".format(username))
+        return None
+    list_of_opponents = jstrisfunctions.opponents_matchups(searched_games.all_stats)
+    embed = await embed_init(username)
+
+    has_opponent = False
+    for key in list_of_opponents:
+        if key is None:
+            continue
+        if key.lower() == opponent.lower():
+            has_opponent = True
+            embed.add_field(name='**opponent:**', value=key, inline=True)
+            embed.add_field(name='**games won:**', value=list_of_opponents[key]["won"], inline=True)
+            embed.add_field(name='**total games:**', value=list_of_opponents[key]["games"], inline=True)
+            embed.add_field(name='**apm:**', value=list_of_opponents[key]["apm"], inline=True)
+            embed.add_field(name='**spm:**', value=list_of_opponents[key]["spm"], inline=True)
+            embed.add_field(name='**pps:**', value=list_of_opponents[key]["pps"], inline=True)
+
+    embed.set_footer(text='All stats here are for the player, not the opponent. To find the opponents stats, simply '
+                          'call this command again in reverse. Also, Jstris will delete replays over time, and they '
+                          'will not be counted here')
+
+    if not has_opponent:
+        await ctx.send(ctx.author.mention)
+        await ctx.send("No found games of {} vs {}.".format(username, opponent))
+        return None
+    await ctx.send(ctx.author.mention)
+    await ctx.send(embed=embed)
 
 
 async def replay_send(ctx, my_ps):
+    embed = await embed_init(my_ps['username'])
+
+    for i in my_ps:
+        if i not in ("username", 'replay'):
+            embed.add_field(name="**{}:**".format(i), value=my_ps[i], inline=False)
     if my_ps["replay"] != "- ":
-        embed = discord.Embed(
-            title=my_ps['username'],
-            url=my_ps['replay'],
-            color=discord.Colour.red())
+        embed.add_field(name="**replay:**", value=my_ps['replay'], inline=False)
     else:
-        embed = discord.Embed(
-            title=my_ps['username'],
-            color=discord.Colour.red())
+        embed.add_field(name="**replay:**", value="replay not available", inline=False)
+    embed.set_footer(text='Have any suggestions? Please message Truebulge#0358 on Discord!')
+    await ctx.send(ctx.author.mention)
+    await ctx.send(embed=embed)
+
+
+async def embed_init(username):
+    embed = discord.Embed(
+        title=username,
+        url="https://jstris.jezevec10.com/u/{}".format(username),
+        color=discord.Colour.red())
     embed.set_author(name="BadgerBot")
     embed.set_thumbnail(url="https://i.imgur.com/WDUv9f0.png")
-    for i in my_ps:
-        if i != "username":
-            embed.add_field(name=i, value=my_ps[i], inline=False)
-    await ctx.send(embed=embed)
+    return embed
 
 if __name__ == "__main__":
 
